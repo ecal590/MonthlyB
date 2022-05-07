b3f5eff911f1824303bb28fa84f137b2e42b233c
Ebony <ecal590@gmail.com>
2022-05-07 19:36:55 -0400

    push test

--- login/login_screen.py
+++ login/login_screen.py
@@ -1,2 +1,9 @@
-for i in range(6):
-    print(i)
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+form = ttk.Frame(root, padding= 10)
+form.grid()
+ttk.Label(form, text="Location").grid(column=0, row=0)
+ttk.Button(form, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
